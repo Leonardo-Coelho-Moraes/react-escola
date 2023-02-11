a90e18ebf9e37f6d4b10c3b698b64102be2886e1
Leonardo Coelho Moraes <108236132+Leonardo-Coelho-Moraes@users.noreply.github.com>
2023-02-11 11:23:58 -0400

    Update Cursos.jsx

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -1,30 +1,16 @@
 import { Link } from 'react-router-dom';
 import styles from '../css/Cursos.module.css';
 import cursos from '../data';
-import Loading from '../components/Loading';
-import { useState, useEffect } from 'react';
+
 
 export default function Cursos() {
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-  
-      setList(cursos);
-      setLoading(false);
-   
-  },);
-
-  if (loading) {
-    return <Loading />;
-  }
-
+ 
 return (
 <section className={styles.section2}>
   <h2>Cursos Técnicos</h2>
   <div className="container">
     <div className={styles.cursos}>
-      {list.map((curso) => {
+      {cursos.map((curso) => {
       return (
       <Link to={`/cursos/${curso.name}`} className={styles.curso}><img className={styles.image} src={curso.image}
         alt={curso.name} /><span className={styles.nome}>{curso.name} </span></Link>
